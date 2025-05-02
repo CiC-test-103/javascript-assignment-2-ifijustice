e6e116aa255a7e22285f7f5ef7ad9170df15f7f4
ifijustice <42317064+ifijustice@users.noreply.github.com>
2025-05-02 15:46:58 -0300

    Update Bank.js

--- Bank.js
+++ Bank.js
@@ -5,9 +5,12 @@ class Bank {
         this.accounts = []; // Stores all accounts in the bank
     }
 
-    // Add methods here:
-    // Example: createAccount(name, initialDeposit)
-
+    // Create a new account and add it to the bank's list
+    createAccount(name, initialDeposit) {
+        const account = new Account(name, initialDeposit);
+        this.accounts.push(account);
+        return account;
+    }
 }
 
 // Account Class: Represents a single user's account
@@ -18,19 +21,35 @@ class Account {
         this.transactionHistory = []; // Keeps a record of all transactions
     }
 
-    // Add methods here:
-    // Example: deposit(amount) 
-    // example data to be stored in transactionHistory { transactionType: 'Deposit', amount: 500 }
+    deposit(amount) {
+        this.balance += amount;
+        this.transactionHistory.push({ transactionType: 'Deposit', amount });
+    }
 
-    // Example: withdraw(amount)
-    // example data to be stored in transactionHistory { transactionType: 'Withdrawal', amount: 200 }
+    withdraw(amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            this.transactionHistory.push({ transactionType: 'Withdrawal', amount });
+        } else {
+            console.log('Insufficient funds');
+        }
+    }
 
-    // Example: transfer(amount, recipientAccount)
-    // example data to be stored in transactionHistory:
-    // for account sending { transactionType: 'Transfer', amount: 300, to: recipientName }
-    // for account recieving { transactionType: 'Received', amount: 300, from: senderName }
-    
-    // Example: checkBalance()
+    transfer(amount, recipientAccount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            recipientAccount.balance += amount;
+
+            this.transactionHistory.push({ transactionType: 'Transfer', amount, to: recipientAccount.name });
+            recipientAccount.transactionHistory.push({ transactionType: 'Received', amount, from: this.name });
+        } else {
+            console.log('Insufficient funds for transfer');
+        }
+    }
+
+    checkBalance() {
+        return this.balance;
+    }
 }
 
 //<-------------------------------DO NOT WRITE BELOW THIS LINE------------------------------>
@@ -69,6 +88,5 @@ function testBankOperations() {
 module.exports = testBankOperations;
 
 //<-------------------------------DO NOT WRITE ABOVE THIS LINE------------------------------>
-
 
 console.log(testBankOperations());
